5792261dd90c39f355e71781f9f60727e22449db
Georgiy <Ger900005@mail.ru>
2025-06-13 10:50:57 +0700

    ````

---
+++ InterviewPrepApi/Services/QuestionService.cs
@@ -0,0 +1,53 @@
+﻿using InterviewPrepApi.Data;
+using InterviewPrepApi.DTOs;
+using InterviewPrepApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InterviewPrepApi.Services;
+
+public class QuestionService
+{
+    private readonly AppDbContext _context;
+
+    public QuestionService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<QuestionReadDto>> GetAllAsync()
+    {
+        return await _context.Questions
+            .Select(q => new QuestionReadDto
+            {
+                Id = q.Id,
+                Text = q.Text,
+                Category = q.Category,
+                Answer = q.Answer,
+                Difficulty = q.Difficulty,
+            })
+            .ToListAsync();
+    }
+
+    public async Task<QuestionReadDto> CreateAsync(QuestionCreateDto dto)
+    {
+        var question = new Question
+        {
+            Text = dto.Text,
+            Answer = dto.Answer,
+            Category = dto.Category,
+            Difficulty = dto.Difficulty
+        };
+
+        _context.Questions.Add(question);
+        await _context.SaveChangesAsync();
+
+        return new QuestionReadDto
+        {
+            Id = question.Id,
+            Text = question.Text,
+            Answer = question.Answer,
+            Category = question.Category,
+            Difficulty = question.Difficulty
+        };
+    }
+}
